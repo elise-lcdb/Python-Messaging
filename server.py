8672a5974a3db24d44c7ff537f55c0c7fbd07bc2
Utilisateur invité <Guest@s23-2.local>
2019-03-19 18:37:19 +0100

    :fire: Intial commit

---
+++ server.py
@@ -0,0 +1,18 @@
+import os
+from socket import *
+
+host = ""
+port = 13000
+buf = 1024
+addr = (host, port)
+UDPSock = socket(AF_INET, SOCK_DGRAM)
+UDPSock.bind(addr)
+print ("Waiting to recieve messages...")
+while True:
+    (data, addr) = UDPSock.recvfrom(buf)
+    print ("Recieved message:" + data.decode("utf-8"))
+    if data == "exit":
+        break
+UDPSock.close()
+os._exit(0)
+
